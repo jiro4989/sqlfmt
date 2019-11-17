b0db374d283bfab5e05eff50b4ceee53ab56bcc0
jiro4989 <jiroron666@gmail.com>
2019-11-17 18:12:07 +0900

    AND対応

--- src/sqlfmt.nim
+++ src/sqlfmt.nim
@@ -1,15 +1,18 @@
 import parsesql, tables
-import sqlfmtpkg/submodule
+from strformat import `&`
+from strutils import toUpperAscii
 
 const
   reservedWords = {
     "lower": {
       nkSelect: "select",
       nkFrom: "from",
+      nkWhere: "where",
     }.toTable,
     "upper": {
       nkSelect: "SELECT",
       nkFrom: "FROM",
+      nkWhere: "WHERE",
     }.toTable,
   }.toTable
 
@@ -27,6 +30,15 @@ proc parsing(node: SqlNode, ret: var string) =
     ret.add("\n  " & reservedWords["upper"][node.kind])
     for node in node.sons:
       parsing(node, ret)
+  of nkWhere:
+    ret.add("\n " & reservedWords["upper"][node.kind])
+    for node in node.sons:
+      parsing(node, ret)
+  of nkInfix:
+    let s = node.sons
+    echo "Infix: " & $s
+    let p = s[0].`$`.toUpperAscii
+    ret.add(&" {s[1]} {p} {s[2]}")
   else:
     echo "Sons: ", node.sons
     for node in node.sons:
@@ -40,14 +52,3 @@ proc formatSQL*(node: SqlNode, lower = false, upper = true): string =
 
 proc formatSQL*(sql: string, lower = false, upper = true): string =
   parseSQL(sql).formatSQL(lower = lower, upper = upper)
-
-echo formatSQL("SELECT * FROM test")
-echo "--------------------"
-echo formatSQL("SELECT id, name, gender, email FROM test WHERE id = 1")
-echo "--------------------"
-echo formatSQL("SELECT id, name, gender, email FROM test WHERE name = 'taro'")
-echo "--------------------"
-echo formatSQL("SELECT u.name, u.hobby FROM user AS u JOIN hobby AS h ON u.id = h.user_id")
-
-when isMainModule:
-  echo(getWelcomeMessage())
--- tests/tsqlfmt.nim
+++ tests/tsqlfmt.nim
@@ -2,12 +2,14 @@ import unittest
 
 include sqlfmt
 
-suite "proc formatSQL(SqlNode)":
-  test "Simple SQL":
-    discard
-
 suite "proc formatSQL(string)":
-  test "Simple SQL":
+  test "SELECT * FROM test_table":
     check formatSQL("SELECT * FROM test_table") == "" &
       "SELECT *\n" &
       "  FROM test_table"
+  test "SELECT * FROM test_table WHERE id = 1":
+    check formatSQL("SELECT * FROM test_table WHERE id = 1 AND name = 'bob'") == "" &
+      "SELECT *\n" &
+      "  FROM test_table\n" &
+      " WHERE id = 1 AND name = 'bob'"
+
